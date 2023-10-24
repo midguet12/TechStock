b0cb77ee91bd0a081a3c07ef00869ed5c5ec765f
aavp0 <aavp0@DESKTOP-GOQF8BF>
2023-10-24 08:23:10 -0600

    CRUD en DAO de Centro Computo 100%

--- src/main/java/com/example/techstock/dao/CentroComputoDAO.java
+++ src/main/java/com/example/techstock/dao/CentroComputoDAO.java
@@ -5,43 +5,99 @@ import com.example.techstock.domain.EquipoComputo;
 import com.example.techstock.dao.DBConnection;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CentroComputoDAO {
     String url = DBConnection.getUrl();
     String user = DBConnection.getUser();
     String password = DBConnection.getPassword();
 
-    /*public boolean create(CentroComputo centroComputo){
-        Boolean success = false;
-        String nombre = centroComputo.getNombre();
-        Integer equipoDeComputo = centroComputo.getEquipoDeComputo();
-        Integer perifericos = centroComputo.getPerifericos();
-
-        try{
-            Connection connection = DriverManager.getConnection(url, user, password);
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("INSERT INTO equipo_computo(nombre, equipoDeComputo,perifericos ) values (?,?,?)");
-
-        } catch (Exception e){
-            System.out.println(e.getMessage());
-        }
-
-        return success;
-    }*/
-
     public boolean create(CentroComputo centroComputo) throws SQLException {
-        String sql = "INSERT INTO centro_computo (nombre) VALUES (?)";
+        String query = "INSERT INTO centro_computo (nombre) VALUES (?)";
         try (Connection connection = DriverManager.getConnection(url, user, password);
-             PreparedStatement statement = connection.prepareStatement(sql)) {
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
 
-            statement.setString(1, centroComputo.getNombre());
+            preparedStatement.setString(1, centroComputo.getNombre());
 
-            int filasInsertadas = statement.executeUpdate();
+            int filasInsertadas = preparedStatement.executeUpdate();
 
             return filasInsertadas > 0;
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return false;
         }
+    }
+
+    public boolean update(CentroComputo centroComputo) throws SQLException {
+        String query = "UPDATE centro_computo SET  nombre = ?  WHERE numeroSerie = ?";
+        String nombre = centroComputo.getNombre();
+
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setString(1, centroComputo.getNombre());
+
+            int rows = preparedStatement.executeUpdate();
+            if (rows>0){
+                System.out.println("Se actualizaron " + rows + " lineas.");
+                return true;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return false;
+    }
+
+    public boolean delete(String nombre){
+        String query = "DELETE from centro_computo WHERE nombre = ?";
+        try{
+            Connection connection = DriverManager.getConnection(url, user, password);
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setString(1, nombre);
+
+            int rows = preparedStatement.executeUpdate();
+            if (rows>0){
+                System.out.println("Se eliminaron " + rows + " filas");
+                return true;
+            }
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return false;
+    }
+
+    public List<CentroComputo> readAll(){
+        String query = "SELECT * FROM centro_computo";
+
+        try{
+            List<CentroComputo> listaCentroComputo = new ArrayList<CentroComputo>();
+
+            Connection connection = DriverManager.getConnection(url, user, password);
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()){
+
+                CentroComputo centroComputo = new CentroComputo();
+                centroComputo.setNombre(resultSet.getString("nombre"));
+                listaCentroComputo.add(centroComputo);
+
+            }
+
+            System.out.println("Se obtuvieron " + listaCentroComputo.size() + " elementos.");
+
+            return listaCentroComputo;
+
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+
     }
 }
