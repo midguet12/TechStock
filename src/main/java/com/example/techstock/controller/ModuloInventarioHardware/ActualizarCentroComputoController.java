f663ecd8a3871f9b81ac853ae5f6291351a7b90f
Andrea29-18 <aavp1603@hotmail.com>
2023-11-06 08:19:29 -0600

    Creando esqueleto de consultar

---
+++ src/main/java/com/example/techstock/controller/ModuloInventarioHardware/ActualizarCentroComputoController.java
@@ -0,0 +1,11 @@
+package com.example.techstock.controller.ModuloInventarioHardware;
+
+import javafx.event.ActionEvent;
+
+public class ActualizarCentroComputoController {
+    public void btnAceptar(ActionEvent actionEvent) {
+    }
+
+    public void btnCancelar(ActionEvent actionEvent) {
+    }
+}
